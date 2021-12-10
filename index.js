94d8791e15f04f1019eb944aa1a8ecc0f775a664
Keith Hon <keith.wkhon@gmail.com>
2021-12-10 11:50:53 +0800

    added codes to transfer all repos from organization

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 const fs = require('fs');
-
 const axios = require('axios');
+const isReadFromLocaleFile = true;
+const isOldOwnerOrganization = false;
 
 async function transfer({ repo }) {
-
     var data = JSON.stringify({
         "new_owner": process.env.NEW_GITHUB_ACCOUNT_ID,
     });
@@ -19,14 +19,47 @@ async function transfer({ repo }) {
         data: data
     };
     return axios(config);
+
+}
+
+async function getAllOrgRepos({ org }) {
+
+    let repos = [];
+    let limit = 100;
+    let shouldContinue = true;
+    let page = 1;
+
+    while (shouldContinue) {
+        console.log(`Getting data from page ${page}`);
+        var config = {
+            method: 'get',
+            url: `https://api.github.com/orgs/${org}/repos?per_page=${limit}&page=${page}`,
+            headers: {
+                'Authorization': `Basic ${Buffer.from(process.env.OLD_GITHUB_ACCOUNT_ID + ":" + process.env.GITHUB_PERSONAL_ACCESS_TOKEN).toString('base64')}`
+            }
+        };
+        let response = await axios(config);
+        let list = response.data;
+        repos.push(...list.map(repo => repo.name));
+        page++;
+        if (list.length < limit) {
+            shouldContinue = false;
+        }
+    }
+
+    return repos;
 }
 
 (async () => {
 
-    let repos = fs.readFileSync('./repos.txt', 'utf8').split('\n');
+    let repos;
+    if (isReadFromLocaleFile) {
+        repos = fs.readFileSync('./repos.txt', 'utf8').split('\n');
+    } else if (isOldOwnerOrganization) {
+        repos = await getAllOrgRepos({ org: process.env.OLD_GITHUB_ACCOUNT_ID });
+    }
 
     for (const [_, repo] of repos.entries()) {
-
         try {
             await transfer({ repo });
             console.log(`${repo} successfully transferred from ${process.env.OLD_GITHUB_ACCOUNT_ID} to ${process.env.NEW_GITHUB_ACCOUNT_ID}`);
@@ -34,5 +67,5 @@ async function transfer({ repo }) {
             console.log(`${repo} failed: ${err}`);
         }
     }
-    
+
 })();
